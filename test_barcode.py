ecd18ed6b3f2e67d420c9c52e80dfb7c8a336d0e
miku_note_win <miku.f.810129@gmail.com>
2022-05-28 14:30:56 +0900

    Supports two-column barcodes

--- test_barcode.py
+++ test_barcode.py
@@ -5,7 +5,7 @@ import time
 
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
-
+barcodes = []
 def ReadBarcode():
     while cap.isOpened():
         time.sleep(1)
@@ -16,9 +16,9 @@ def ReadBarcode():
             if decoded_frame:
                 for barcode in decoded_frame:
                     barcodeData = barcode.data.decode('utf-8')
+                    barcodes.append(barcodeData)
                     #winsound.Beep(2000, 50)
                     font_color = (0, 0, 255)
-                    number = barcodeData
                     x, y, w, h = barcode.rect
                     cv2.rectangle(frame, (x, y), (x + w, y + h), font_color, 2)
                     #frame = cv2.putText(frame, barcodeData, (x, y - 10), font, .5, font_color, 2, cv2.LINE_AA)
@@ -27,6 +27,10 @@ def ReadBarcode():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
+    number = 0
+    for i in range(len(barcodes)):
+        if int(barcodes[i])>number:
+            number = int(barcodes[i])
     return number
 
 
