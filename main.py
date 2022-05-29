0f3f9f926f9e2178ea73c75b5a5cce775cc64d6f
miku_note_win <miku.f.810129@gmail.com>
2022-05-29 14:32:28 +0900

    add get method

---
+++ main.py
@@ -0,0 +1,35 @@
+from distutils.command.upload import upload
+import os
+import logging
+import pathlib
+import json
+import sqlite3
+import hashlib
+from fastapi import FastAPI, Form, HTTPException,File,UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import test_api
+
+app = FastAPI()
+logger = logging.getLogger("uvicorn")
+logger.level = logging.INFO
+images = pathlib.Path(__file__).parent.resolve() / "images"
+origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET","POST","PUT","DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Hello, world!"}
+
+@app.get("/items/barcode/{barcode_num}")
+def show_item_info(barcode_num):
+    result_json = test_api.GetInfo(barcode_num)
+    result = test_api.GetInfo(result_json)
+    return result
+
