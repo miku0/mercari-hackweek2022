c44c57af446ee20705bc302d0f28265a9c0889a4
miku_note_win <miku.f.810129@gmail.com>
2022-05-29 14:46:40 +0900

    change the function

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from fastapi import FastAPI, Form, HTTPException,File,UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import test_api
+import test_barcode
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -27,9 +28,17 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+'''
 @app.get("/items/barcode/{barcode_num}")
 def show_item_info(barcode_num):
     result_json = test_api.GetInfo(barcode_num)
     result = test_api.GetInfo(result_json)
     return result #{'name': name, 'category':category,'description': description,'transportation_method':transportation_method}
+'''
 
+@app.get("/items/barcode")
+def get_item():
+    isbn = test_barcode.ReadBarcode()
+    result_json = test_api.GetInfo(isbn)
+    result = test_api.GetInfo(result_json)
+    return result #{'name': name, 'category':category,'description': description,'transportation_method':transportation_method}
